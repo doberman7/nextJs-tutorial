9dcc360f097a4199b3cfbc59373701d2a5bed83e
doberman7 <ivanrubioangeles@gmail.com>
2021-02-17 17:53:26 -0600

    _app.js This App component is the top-level component which will be common across all the different pages.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,30 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+const YourComponent = () => (
+  <Image
+    src="/images/d7.png" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 };
 
