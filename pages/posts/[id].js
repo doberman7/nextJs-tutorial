069641f35fbc5da54bdec23cc604505978dc80e4
doberman7 <ivanrubioangeles@gmail.com>
2021-02-24 14:35:41 -0600

    implement get statics props

---
+++ pages/posts/[id].js
@@ -0,0 +1,36 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {" "}
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+  const paths = getAllPostIds();
+  return {
+    //paths contains the array of known paths returned by getAllPostIds(), which include the params defined by pages/posts/[id].js
+    paths,
+    fallback: false,
+  };
+}
