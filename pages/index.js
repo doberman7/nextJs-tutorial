9686dc9cddf6f883ef98e988e5d147f7b983fc9e
doberman7 <ivanrubioangeles@gmail.com>
2021-02-24 18:38:37 -0600

    links

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
+import Link from "next/link";
+import Date from "../components/date";
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   //call a joke from api
@@ -35,11 +38,13 @@ export default function Home({ allPostsData, chistes }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
+import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -14,23 +15,20 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {/* Add this <Head> tag */}
       <Head>
-        {/* Replace {postData.date} with this */}
-        <Date dateString={postData.date} />
-
-        {/* <title>{postData.title}</title> */}
-      </Head>{" "}
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
+
 export async function getStaticPaths() {
   // Return a list of possible value for id
   const paths = getAllPostIds();
